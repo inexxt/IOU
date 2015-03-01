b133f76f05069f9b0ed431b18bfe169f4d09ceb1
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2015-03-01 01:28:09 +0000

    new password

--- database.php
+++ database.php
@@ -3,6 +3,7 @@
 function connect_db()
 {
 	$con = mysql_connect('localhost','root','');
+	$con = mysql_connect('localhost','root','ee==mmcc22');
 	if (!$con) {
 		die('Could not connect: ' . mysqli_error($con));
 	}
--- index.php
+++ index.php
@@ -1,45 +1,51 @@
 <html>
 <body>
-<?php
+	<?php
 
-session_start();
+		session_start();
 
-define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__ . '/fb/');
-require __DIR__ . '/autoload.php';
+		define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__ . '/fb/');
+		require __DIR__ . '/autoload.php';
 
-use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookRedirectLoginHelper;
+		use Facebook\FacebookSession;
+		use Facebook\FacebookRequest;
+		use Facebook\GraphUser;
+		use Facebook\FacebookRequestException;
+		use Facebook\FacebookRedirectLoginHelper;
 
-FacebookSession::setDefaultApplication('360281824177162','121b16cc09ab6bcfa05cc43db1b0350a');
+		FacebookSession::setDefaultApplication('360281824177162','121b16cc09ab6bcfa05cc43db1b0350a');
 
-$req = $_REQUEST;
+		$req = $_REQUEST;
 
-if(array_key_exists("action", $req) && array_key_exists("accessToken", $req))
-{
-	$accessToken = $req["accessToken"];
-	$session = new FacebookSession($accessToken);
-	switch($req["action"])
-	{
-		case "AUTH":
-			if(is_null($session))
-				printf("failed to authenticate");
-			else
-			{  
-				$me = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
-				$result = array_merge(deal(array("ACTION"=>"PROFILE", "UID"=>$me->getId()), $session), deal(array("ACTION"=>"LOANS_LIST", "UID"=>$me->getId()), $session));
-				echo json_encode($result);
-			} 
-			break;
-		default:
-			$me = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
-			deal(array("ACTION"=>strtoupper($req["action"]), "UID"=>$me->getId(), $session);
-			break;
-	}
-}
+		if(array_key_exists("ACTION", $req) && array_key_exists("ACCESSTOKEN", $req))
+		{
+			$accessToken = $req["ACCESSTOKEN"];
+			$session = new FacebookSession($accessToken);
+			switch($req["ACTION"])
+			{
+				case "AUTH":
+					if(is_null($session))
+						printf("failed to authenticate");
+					else
+					{  
+						$me = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
+						$req["ACTION"] = "PROFILE";
+						$req["USER"] = $me;
+						$result = deal($req, $session);
+						$req["ACTION"] = "LOANS_LIST";
+						$result2 = deal($req, $session);
+						echo json_encode(array_merge($result, $result2));
+					} 
+					break;
+					
+				default:
+					$me = (new FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(GraphUser::className());
+					$req["USER"] = $me;
+					deal($req, $session);
+					break;
+			}
+		}
 
-?>
+	?>
 </body>
 </html>
